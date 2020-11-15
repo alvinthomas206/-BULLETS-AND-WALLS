ce7f78ba6daeb202331d0562a4d1a2f081296c89
ALVIN THOMAS <alvinthomas206@gmail.com>
2020-11-15 12:12:58 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,53 +1,59 @@
-var thickness, wall;
-var bullet, speed, weight;
-
+var wall, thickness;
+var bullet,speed, weight;
 
 function setup() {
-  createCanvas(1600,400);
- 
-  speed = random(223,321);
+  createCanvas(1600, 400);
 
-  weight = random(30,52)
-  bullet = createSprite(50,200,20,10);
-  bullet.shapeColor = (255,255,255);
-  thickness=random(22,83);
-  wall = createSprite(1200,200,thickness,height/2);
-  wall.shapeColor = (80,80,80) ;
-  bullet.velocityX = speed;
-  wall.setCollider("rectangle", 100,0,thickness+200, height);
+  speed=random(223,321)
+  weight=random(30,52)
+
+
+  thickness=random(22,83)
+
+    bullet=createSprite(50, 200, 50,5);  
+    bullet.velocityX = speed;
+    bullet.shapeColor=color(255);
+
+     wall=createSprite(1200, 200, thickness, height/2);  
+
+    wall.shapeColor=color(230,230,230);
+  
 }
 
+
 function draw() {
-  background(200,200,200);  
-  
-  if(wall.x-bullet.x < (bullet.width + wall.width)/2)
-  {
-    bullet.collide(wall);
-    bullet.velocityX=0;
-    var damage = 0.5* weight * speed* speed/(thickness*thickness*thickness);
-    if(damage>10)
-    {
-      wall.shapeColor = color(255,0,0);
-    }
-    
-    if(damage<=10)
-    {
-      wall.shapeColor = color(0,255,0); 
-    }
-    
-    
-    
-    
-    
-  
-  
-  
-  
-  
-  
-  
+  background(0);  
+ 
+  if(hasCollided(bullet, wall)) {
+  	bullet.velocityX=0;
+  	var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
+
+  	
+	if(damage>10){
+		wall.shapeColor=color(255,0,0);
+		
+	}
+
+	
+
+	if(damage<10){
+		wall.shapeColor=color(0,255,0);
+	}
+	
   }
+
+
   drawSprites();
+ 
+}
 
 
+function hasCollided(lbullet, lwall){
+	bulletRightEdge=lbullet.x +lbullet.width;
+	wallLeftEdge=lwall.x;
+	if (bulletRightEdge>=wallLeftEdge)
+	{
+		return true
+	}
+	return false;
 }
